66425a9dbcef70af620f8eab938d3e4e8d2a2496
TrueNewBee <1076176004@qq.com>
2021-01-14 17:23:51 +0800

    day05

--- day05/01fuxi/main.go
+++ day05/01fuxi/main.go
@@ -1,1 +1,120 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// 结构体
+
+type temp struct {
+	x int
+	y int
+}
+
+type person struct {
+	name string
+	age  int
+}
+
+// 普通有返回函数
+// 返回值是对应的int类型
+func sum(x int, y int) int {
+	return x + y
+}
+
+// 构造(结构体变量的)函数: 由于每次都实例化结构体麻烦,故封装到函数里面,叫构造函数
+// 返回值是对应的结构体类型
+func newPerson(n string, i int) person {
+	return person{
+		name: n,
+		age:  i,
+	}
+}
+
+// 方法
+// 接受者使用对应类型的首字母小写
+// 指定了接受者之后,只有接受者这个类型的变量才能调用这个方法
+func (p person) dream(str string) {
+	fmt.Printf("%s的梦想是学好go语言,然后%s", p.name, str)
+}
+
+// func (p person) guonian() {
+// 	p.age++ // 此处是p1的副本,改的是副本
+// }
+
+// 指针接受者
+// 1. 需要修改结构体变量的值时需要用指针接受者
+// 2. 结构体本身比较大,拷贝的内存开销比较大时也要使用指针接受者
+// 3. 保持一致性:如果一个方法使用了指针接受者,其他的方法为了统一也要使用指针接受者
+func (p *person) guonian() {
+	p.age++
+}
+
+func main() {
+	//普通类型
+	var a = temp{
+		1,
+		2,
+	}
+	// 匿名结构体
+	var b = struct {
+		x int
+		y int
+	}{10, 20}
+	fmt.Println(a)
+	fmt.Println(b)
+
+	// 对比记忆 基本数据类型和自定义数据类型
+	// 基本数据类型使用
+	var x int
+	y := int8(18)
+	fmt.Println(x, y)
+
+	// 自定义数据类型使用
+	var p1 person // 结构体实例化1 :一个一个调用传值
+	p1.name = "周林"
+	p1.age = 90000
+	p2 := person{"保德路", 18}   // 结构体实例化2 : 用{} 一次写完(少可以用)
+	p3 := newPerson("周林", 13) // 结构体实例化3 : 用构造函数直接用完! (多次使用推荐)
+	fmt.Println(p1, p2, p3)
+	p1.dream("好好学习")
+	p2.dream("天天向上")
+
+	fmt.Println(p1.age)
+	p1.guonian()
+	fmt.Println(p1.age)
+
+	// 结构体嵌套
+	type addr struct {
+		province string
+		city     string
+	}
+	type student struct {
+		name string
+		addr // 匿名嵌套别的结构体,就使用类型名做名称
+	}
+
+	type point struct {
+		X int `json:"zhoulin"` // 给json使用的名字
+		Y int `json:"baodelu"`
+	}
+
+	// 序列化
+	q1 := point{100, 12}
+	b1, err := json.Marshal(q1)
+	// 如果出错了
+	if err != nil {
+		fmt.Printf("marshal failed, err:%v\n", err)
+	}
+	fmt.Println(string(b1))
+
+	// 反序列化: 有字符串---->Go中的结构体变量
+	str1 := `{"zhoulin":10010,"baodelu":10086}`
+	var po2 point                            // 造一个结构体变量,准备接受反序列化的值
+	err = json.Unmarshal([]byte(str1), &po2) // 需要传入个指针,修改值
+	if err != nil {
+		fmt.Printf("unmarshal failed, err:%v\n", err)
+	}
+	fmt.Println(po2)
+}
--- day05/README.md
+++ day05/README.md
@@ -2,6 +2,15 @@
 
 # 内容回顾
 
+## 自定义类型和类型别名
+
+```go
+type myInt int  // 自定义类型
+type newInt = int	// 类型别名
+```
+
+**类型别名只在代码编写过程中有效,编译完之后就不存在了,内置的*byte和rune都属于类型别名**
+
 ## 结构体
 
 基本的数据类型: int float等表示现实中的物件有局限性
@@ -15,10 +24,16 @@ var name = "保德路"
 结构体是一种数据类型,一种我们自己造的可以保存多个维度数据类型
 
 ```go
+// 有名字的结构体
 type person struct{
     name string
     age int
     addr string
+}
+var a = person{
+    "元帅",
+    12,
+    "沙河",
 }
 ```
 
@@ -29,19 +44,75 @@ type person struct{
 多用于临时场景
 
 ```go
-
+// 普通 有名字结构体
+var temp = struct {
+		x int
+		y int
+	}
+var a = temp{
+    1,
+    2,
+}
+// 匿名结构体
+var b = struct {
+		x int
+		y int
+	}{10, 20}
 ```
 
 
 
 ## 构造函数
 
+```go
+// 结构体类型为person
+type person struct {
+	name string
+	age  int
+}
+// 构造(结构体变量的)函数: 由于每次都实例化结构体麻烦,故封装到函数里面,叫构造函数
+// 返回值是对应的结构体类型
+func newPerson(n string, i int) person {
+	return person{
+		name: n,
+		age:  i,
+	}
+}
+func main (){
+    //用构造函数直接用完! (多次使用,结构体字段多,推荐)
+    p3 := newPerson("周林", 13) 
+	fmt.Println(p3)
+}
+```
+
+![image-20210114113433181](D:\Go\src\chentianxiang.vip\studygo\day05\README.assets\image-20210114113433181.png)
+
 ## 方法和接受者
+
+方法是有接受者的函数,接受者指的是哪个类型的变量可以调用这个函数
+
+![image-20210114154419827](D:\Go\src\chentianxiang.vip\studygo\day05\README.assets\image-20210114154419827.png)
+
+结构体是值类型
+
+![image-20210114162925396](D:\Go\src\chentianxiang.vip\studygo\day05\README.assets\image-20210114162925396.png)
 
 ## 结构体的嵌套
 
+![image-20210114163350505](D:\Go\src\chentianxiang.vip\studygo\day05\README.assets\image-20210114163350505.png)
+
 ## 结构体的匿名字段
+
+![image-20210114163303210](D:\Go\src\chentianxiang.vip\studygo\day05\README.assets\image-20210114163303210.png)
 
 ## JSON序列化与反序列化
 
- 
+ 经常出现问题:
+
+​	1.结构体内部的字段要大写!!!!不大写别人是访问不到的
+
+​	2. 反序列化时要传递指针!
+
+![image-20210114163707782](D:\Go\src\chentianxiang.vip\studygo\day05\README.assets\image-20210114163707782.png)
+
+![image-20210114164815021](D:\Go\src\chentianxiang.vip\studygo\day05\README.assets\image-20210114164815021.png)
